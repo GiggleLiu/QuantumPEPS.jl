192e18e8941ffa9ef73245e1ae88da68e8e16196
GiggleLiu <cacate0129@gmail.com>
2019-07-02 09:52:58 +0800

    update data

--- data/decoder.jl
+++ data/decoder.jl
@@ -9,9 +9,8 @@ function load_training(filename)
     #res["qopt"], res["loss"], res["params"]
 end
 
-function decode(nx::Int, ny::Int, d::Int=5)
+function decode(nx::Int, ny::Int, depth::Int=5)
     nv = 1
-    depth = 2
     suff = "j1j2-nx$nx-ny$ny-nv$nv-d$depth"
     res = load_training("$suff.jld2")
     writedlm("$suff-params.dat", res["params"])
