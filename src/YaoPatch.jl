de44da4a5ce6306028c58d6cac9a91dd3b1f7b70
GiggleLiu <cacate0129@gmail.com>
2022-06-03 22:24:07 -0400

    update Yao version

--- src/YaoPatch.jl
+++ src/YaoPatch.jl
@@ -1,25 +1,3 @@
-function Yao.apply!(reg::ArrayReg, pb::PutBlock{D,2,RotationGate{2,T,G}}) where {D,T,G<:SWAPGate}
-    mask1 = bmask(pb.locs[1])
-    mask2 = bmask(pb.locs[2])
-    mask12 = mask1|mask2
-    a, c, b_, d = mat(Rx(pb.content.theta))
-    e = exp(-im/2*pb.content.theta)
-    state = statevec(reg)
-    for b in basis(reg)
-        if b&mask1==0
-            i = b+1
-            i_ = b ⊻ mask12 + 1
-            if b&mask2==mask2
-                u1rows!(state, i, i_, a, b_, c, d)
-            else
-                mulrow!(state, i, e)
-                mulrow!(state, i_, e)
-            end
-        end
-    end
-    return reg
-end
-
 mutable struct Bag{D}<:TagBlock{AbstractBlock, D}
     content::AbstractBlock{D}
 end
@@ -27,7 +5,7 @@ end
 Yao.content(bag) = bag.content
 Yao.chcontent(bag::Bag, content) = Bag(content)
 Yao.mat(::Type{T}, bag::Bag) where T = mat(T, bag.content)
-YaoBlocks._apply!(reg::AbstractRegister, bag::Bag) = apply!(reg, bag.content)
+YaoBlocks.unsafe_apply!(reg::AbstractRegister, bag::Bag) = YaoBlocks.unsafe_apply!(reg, bag.content)
 YaoBlocks.PreserveStyle(::Bag) = YaoBlocks.PreserveAll()
 setcontent!(bag::Bag, content) = (bag.content = content; bag)
 
